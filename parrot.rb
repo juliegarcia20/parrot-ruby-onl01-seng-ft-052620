49e0622ee9a5c60814be6b3b1b18404d4b7e93f9
Julie Garcia <juliegarcia.cali@gmail.com>
2020-05-30 01:26:45 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,12 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot = "Squawk!"
-  return phrase 
+def parrot
+  phrase = "Squawk!"
+  puts phrase 
+end 
+
+def parrot
+  return phrase
 end 
   
   
